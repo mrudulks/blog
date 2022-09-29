4925bc6065043a413f97c9dadb4a9c3d50db2ad8
mrudulks <mrudulcap@gmail.com>
2022-09-29 16:19:54 +0530

    new todo app

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <header class="bg-dark pt-3 pb-3">
+            <div class="container">
+                <h4 class="text-muted m-0">My Todo</h4>
+            </div>
+        </header>
+        <nuxt></nuxt>
+    </main>
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container pt-5">
+    <div class="row">
+      <div class="col-12 mb-3">
+        <div class="d-flex">
+          <input
+            type="text"
+            class="form-control mr-3"
+            placeholder="Add another list"
+            v-model="newList"
+          />
+          <button class="btn btn-info w-25">Add List</button>
+        </div>
+      </div>
+  <ListCard></ListCard>
+    </div>
+  </section>
+</template>
+
+<script>
+import ListCard from "../components/ListCard.vue";
+export default {
+    data() {
+      return{
+        newList: ""
+      }
+    },
+    components: { ListCard }
+}
+</script>
